56f9044ff6b691ae58f22d1e72dda7ea03e299f7
Fábio Henrique <psifabiohenrique@outlook.com>
2025-11-12 19:59:00 -0300

    Corrigido os labels das repostas

--- src/components/quadro_respostas.py
+++ src/components/quadro_respostas.py
@@ -9,11 +9,11 @@ class QuadroRespostas(ctk.CTkFrame):
         self.grid_rowconfigure(3, weight=2)
         self.grid_columnconfigure(0, weight=1)
 
-        ctk.CTkLabel(self, text='Resposta Local').grid(row=0, column=0, sticky='nsew',  padx=10, pady=(10,5))
+        ctk.CTkLabel(self, text='Resposta Original').grid(row=0, column=0, sticky='nsew',  padx=10, pady=(10,5))
         self.caixa_texto_ollama = ctk.CTkTextbox(self, state="disabled", font=("Arial", 14))
         self.caixa_texto_ollama.grid(row=1, column=0, sticky='nsew', padx=10, pady=(10,5))
 
 
-        ctk.CTkLabel(self, text='Resposta Gemini').grid(row=2, column=0,  sticky='nsew', padx=10, pady=(10,5))
+        ctk.CTkLabel(self, text='Resposta Novo Prompt').grid(row=2, column=0,  sticky='nsew', padx=10, pady=(10,5))
         self.caixa_texto_gemini = ctk.CTkTextbox(self, state="disabled", font=("Arial", 14))
         self.caixa_texto_gemini.grid(row=3, column=0, sticky='nsew', padx=10, pady=(10,5))
